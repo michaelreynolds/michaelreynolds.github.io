885f4c7c3680893e9362cf93ff72bbf23e789160
Michael Reynolds <me@michaelreynolds>
2017-03-09 13:45:01 -0500

    update deploy strategy

--- config.rb
+++ config.rb
@@ -55,7 +55,7 @@ end
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
-  # deploy.branch   = 'master' # default: gh-pages
+  deploy.branch   = 'master' # default: gh-pages
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
